05b93dac24fdea5bd67f6c2c698d67ee18cdb269
anthony keogh <siliconirish@gmal.com>
2020-04-23 11:10:21 +0100

    Meaningful messager goes here

--- routes.py
+++ routes.py
@@ -1,36 +1,36 @@
-from flask import Flask
-from flask import Flask, request, render_template
-from pymongo import MongoClient
-from flask_pymongo import PyMongo
+##from flask import Flask
+##from flask import Flask, request, render_template
+##from pymongo import MongoClient
+##from flask_pymongo import PyMongo
 
 #recognizing the app under flask
-app = Flask(__name__)
+##app = Flask(__name__)
 
 
 
-@app.route('/aboutus')
-def aboutus():
-    return render_template("aboutus.html")
+##@app.route('/aboutus')
+##def aboutus():
+##    return render_template("aboutus.html")
 
-@app.route('/dessert')
-def dessert():
-    return render_template("dessert.html")
-
-
-@app.route('/login')
-def login():
-    return render_template("auth/login.html")
+##@app.route('/dessert')
+##def dessert():
+##    return render_template("dessert.html")
 
 
-@app.route('/add_recipe')
-def add_recipe():
-    return render_template("add_recipe.html")
+##@app.route('/login')
+##def login():
+ ##   return render_template("auth/login.html")
 
-@app.route('/main_course')
-def main_course():
-    return render_template("main_course.html")
 
-@app.route('/register')
-def register():
-    return render_template("register.html")
+##@app.route('/add_recipe')
+##def add_recipe():
+##    return render_template("add_recipe.html")
+
+##@app.route('/main_course')
+##def main_course():
+##    return render_template("main_course.html")
+
+##@app.route('/register')
+##def register():
+ ##   return render_template("register.html")
 
