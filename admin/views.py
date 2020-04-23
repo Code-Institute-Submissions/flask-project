63e297305e3c586357d3d0c24f4517590b932011
anthony keogh <siliconirish@gmal.com>
2020-04-23 11:21:15 +0100

    Meaningful messager goes here

--- admin/views.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-from flask import current_app as app
-
-
-# Set up a Blueprint
-admin_bp = Blueprint('admin_bp', __name__,
-                     template_folder='templates',
-                     static_folder='static')
--- home/tests.py
+++
@@ -1,9 +0,0 @@
-#Test-Driven-Development
-
-#(1.) Unit Tests – It is a piece of a code that invokes another piece of code (unit) and checks if an output of that action is the same as the desired output.
-
-#(2.) Integration Tests – It is testing a unit without full control over all parties in the test. They are using one or more of its outside dependencies, such as a database, threads, network, time, etc.
-
-#(3.) Functional Tests – It is testing a slice of a functionality of the system, observing it as a black box and verifying the output of the system to the functional specification.
-
-#(4.) Acceptance Tests – It is testing does the system fulfill expected business and contract requirements. It is considering the system as a black box.
